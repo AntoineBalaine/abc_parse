5df6bb226b300ab3a78190d8229bf4472720b4c4
Antoine Balaine <abalaineaccts@gmail.com>
2023-11-23 15:04:35 -0500

    Error Reporter: test: share btw scan & parse

--- src/tests/ErrorReporter.spec.ts
+++ src/tests/ErrorReporter.spec.ts
@@ -1,4 +1,5 @@
 import chai from "chai";
+import { AbcErrorReporter } from "../ErrorReporter";
 import { Parser } from "../Parser";
 import { Scanner } from "../Scanner";
 import { tuneHeader } from "./RhythmTransform.spec";
@@ -16,4 +17,16 @@ describe("Error Reporter", () => {
     const errors = parser.getErrors();
     expect(errors).to.be.not.empty;
   });
+  it("Error Reporter: Scanner and Parser can share a reporter.", () => {
+    const sample = "~23 a bc\na,,";
+    const abcErrorReporter = new AbcErrorReporter();
+    const tokens = new Scanner(tuneHeader(sample), abcErrorReporter).scanTokens();
+    const parser = new Parser(tokens, sample, abcErrorReporter);
+    const parse = parser.parse();
+    expect(parse).to.be.not.null;
+    if (parse === null) { return; }
+    const errors = abcErrorReporter.getErrors();
+    expect(errors).to.be.not.empty;
+  });
+
 });
